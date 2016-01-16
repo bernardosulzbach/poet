adfe0dcd8c32870f882ab5aee997f69fb2257579
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-01-16 16:18:02 -0200

    First snapshot of make_sentence.py

---
+++ make_sentence.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import random
+import string
+from collections import defaultdict
+
+
+def remove_punctuation(text):
+    table = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+    return text.translate(table)
+
+
+def is_preposition(token):
+    with open('prepositions.txt') as prepositions:
+        return token.lower() in prepositions.read().split()
+
+
+def is_article(token):
+    return token.lower() in ('a', 'an', 'the')
+
+
+def is_good_ending(token):
+    return not is_article(token) and not is_preposition(token)
+
+
+def is_number(token):
+    for character in token:
+        if not character.isdigit():
+            return False
+    return True
+
+
+def remove_numbers(words):
+    # Currently just removes integers
+    result = []
+    for word in words:
+        if not is_number(word):
+            result.append(word)
+    return result
+
+
+def make_sentence(table):
+    # Get a random word from the body
+    sentence = [random.choice(list(table.keys()))[0]]
+    while len(sentence) < 4 or not is_good_ending(sentence[-1]):
+        best_so_far = None
+        for key in table.keys():
+            if key[0] == sentence[-1]:
+                if (best_so_far is None) or (table[key] > best_so_far[1]):
+                    best_so_far = (key[1], table[key])
+        sentence.append(best_so_far[0])
+    return ' '.join(sentence)
+
+
+def main():
+    with open('corpus.txt') as corpus:
+        text = corpus.read()
+        text = text.lower()
+        text = remove_punctuation(text)
+        words = text.split()
+        words = remove_numbers(words)
+        table = defaultdict(lambda: 0)
+        for i in range(len(words) - 1):
+            table[tuple(words[i:i + 2])] += 1
+        for i in range(10):
+            print(make_sentence(table))
+
+
+if __name__ == '__main__':
+    main()
